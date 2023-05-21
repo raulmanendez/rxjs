3172fabc69ad40cfdc5d569ae94de5b127f8458a
Raul Manendez <muk214782@gmail.com>
2023-05-21 12:58:57 +0530

    buffer related operators done

--- src/app/buffer/buffer.component.ts
+++ src/app/buffer/buffer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, buffer, fromEvent, interval, tap } from 'rxjs';
+import { Observable, buffer, bufferCount, bufferTime, bufferToggle, bufferWhen, fromEvent, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-buffer',
@@ -17,6 +17,12 @@ export class BufferComponent implements OnInit {
   ngOnInit(): void {
 
     this.collectBuffer$=fromEvent(document.getElementById('button-collect')!,'click');
+
+    this.bufferCount();
+    this.bufferCount1();
+    this.bufferCount2();
+    this.bufferCount3();
+    this.bufferCount4();
   }
 
   startBuffer() {
@@ -30,8 +36,53 @@ export class BufferComponent implements OnInit {
     })
   }
 
-  collectBuffer() {
-    
+  bufferCountArr1 = new Array();
+  bufferCount() {
+    this.intervalObs$.pipe(bufferCount(5)).subscribe(data => {
+      this.bufferCountArr1=data;
+    })
+  }
+
+  bufferCountArr2 = new Array();
+  bufferCount1() {
+    this.intervalObs$.pipe(bufferCount(5,3)).subscribe(data => {
+      this.bufferCountArr2=data;
+    })
+  }
+
+  bufferCountArr3 = new Array();
+  bufferCount2() {
+    this.intervalObs$.pipe(bufferTime(3000)).subscribe(data => {
+      this.bufferCountArr3=data;
+    })
+  }
+
+  bufferCountArr4 = new Array();
+  bufferCount3() {
+    const openObs$=interval(5000)
+    const closeObs$= (data:number) => interval(1000)
+
+    this.intervalObs$.pipe(bufferToggle(openObs$,closeObs$)).subscribe(data => {
+      this.bufferCountArr4=data;
+    })
+  }
+
+  bufferCountArr5 = new Array();
+  bufferCount4() {
+    let x=0;
+    this.intervalObs$.pipe(
+      tap(i => x=i),
+      bufferWhen(
+      () => {
+         if(x > 3) {
+          return interval(5000)
+         } else {
+          return interval(1000)
+         }
+      }
+    )).subscribe(data => {
+      this.bufferCountArr5=data;
+    })
   }
 
 }
