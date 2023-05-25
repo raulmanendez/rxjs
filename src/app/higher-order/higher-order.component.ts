8ace8202f4f5926e5fa3eff69930c5e59f5cbf49
Raul Manendez <muk214782@gmail.com>
2023-05-25 22:41:53 +0530

    concat switch exhaust

--- src/app/higher-order/higher-order.component.ts
+++ src/app/higher-order/higher-order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { map, mergeMap, mergeMapTo, of } from 'rxjs';
+import { concatMap, concatMapTo, exhaustMap, map, mergeMap, mergeMapTo, of, switchMap, switchMapTo } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -14,13 +14,21 @@ export class HigherOrderComponent implements OnInit {
   ngOnInit(): void {
     this.mergeMapOperator()
     this.mergeMapToOperator()
+
+    this.concatMapOperator()
+    this.concatMapToOperator()
+
+    this.switchMapOperator()
+    this.switchMapToOperator()
+
+    this.exhaustMapOperator()
   }
 
   mergeMapRes=new Array()
   mergeMapOperator(){
 
     of(1,2,3).pipe(
-      mergeMap(ids => ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/${ids}`).pipe(map(response => response.response)))
+      mergeMap(ids => ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/${ids}`).pipe(map(response => response.response)),2)
     ).subscribe(data => {
       this.mergeMapRes.push(data);
     }) 
@@ -30,9 +38,59 @@ export class HigherOrderComponent implements OnInit {
   mergeMapToOperator(){
 
     of(1,2,3).pipe(
-      mergeMapTo(ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/1`).pipe(map(response => response.response)))
+      mergeMapTo(ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/1`).pipe(map(response => response.response)),2)
     ).subscribe(data => {
       this.mergeMapToRes.push(data);
+    }) 
+  }
+
+  concatMapRes=new Array()
+  concatMapOperator(){
+
+    of(1,2,3).pipe(
+      concatMap(ids => ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/${ids}`).pipe(map(response => response.response)))
+    ).subscribe(data => {
+      this.concatMapRes.push(data);
+    }) 
+  }
+
+  concatMapToRes=new Array()
+  concatMapToOperator(){
+
+    of(1,2,3).pipe(
+      concatMapTo(ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/1`).pipe(map(response => response.response)))
+    ).subscribe(data => {
+      this.concatMapToRes.push(data);
+    }) 
+  }
+
+  switchMapRes=new Array()
+  switchMapOperator(){
+
+    of(1,2,3).pipe(
+      switchMap(ids => ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/${ids}`).pipe(map(response => response.response)))
+    ).subscribe(data => {
+      this.switchMapRes.push(data);
+    }) 
+  }
+
+  switchMapToRes=new Array()
+  switchMapToOperator(){
+
+    of(1,2,3).pipe(
+      switchMapTo(ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/1`).pipe(map(response => response.response)))
+    ).subscribe(data => {
+      this.switchMapToRes.push(data);
+    }) 
+  }
+
+  exhaustMapRes=new Array()
+  exhaustMapOperator(){
+
+    of(1,2,3).pipe(
+      exhaustMap(id => ajax(`https://my-json-server.typicode.com/raulmanendez/json-server/posts/${id}`).pipe(map(response => response.response)))
+    ).subscribe(data => {
+      this.exhaustMapRes.push(data);
     }) 
   }
 
